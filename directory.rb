a3e865eec64a951d060c496841e32c19051144a4
Liz Daly <liz7daly@gmail.com>
2020-01-12 17:49:38 +0000

    Refactor interactive menu
    Adds two new methods to reduce lines in `interactive_menu` method. New methods:
    
    - `print_menu`: shows menu options to the user
    - `show_students`: combines print methods to print the list of students to the
      console
    
    Updates students variable to class variable (`@students`) to reduce no of times
    the student variable is passed around as an argument.

--- directory.rb
+++ directory.rb
@@ -1,20 +1,17 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
     #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     #2. read the input and save it into a variable
     selection = gets.chomp
     #3. do what the user has asked
     case selection
       when "1"
-        students = input_students
+        input_students
       when "2"
-        print_header
-        list(students)
-        print_footer(students)
+        show_students
       when "9"
         exit
       when "exit"
@@ -25,25 +22,35 @@ def interactive_menu
   end
 end
 
+#menu methods
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if !@students.empty?
+    print_header
+    print_students
+    #list_by_cohort(students)
+  end
+  print_footer
+end
 
 #input methods
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
-  # create an empty array
-  students = []
-  #get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 
 def print_header
@@ -51,15 +58,15 @@ def print_header
   puts "---------------"
 end
 
-def list(students)
-  students.each_with_index do |student, index|
+def print_students
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
+def print_footer
   puts "---------------"
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
   puts ""
   puts "-" * 80
 end
